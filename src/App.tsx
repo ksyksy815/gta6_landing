ffa93f7e10c8d2145d8a092bb1eafec4b58ac60a
Seoyeon <s.kim@sensory-minds.com>
2025-07-24 21:30:33 +0200

    clean up the boilerplate, add the assets and set up the dependencies

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+
+gsap.registerPlugin(ScrollTrigger)
+
+const App = () => {
+  return (
+    <main>
+      <h1>Welcome</h1>
+    </main>
+  )
+}
+
+export default App
